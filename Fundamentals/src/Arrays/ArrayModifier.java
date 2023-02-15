601fc995d95345255cd7ea138d193910037343fe
Georgi Ivanov <gogata92@pm.me>
2023-02-15 21:24:34 +0100

    Update ArrayModifier.java

--- Fundamentals/src/Arrays/ArrayModifier.java
+++ Fundamentals/src/Arrays/ArrayModifier.java
@@ -1,5 +1,6 @@
 package Arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayModifier {
@@ -18,32 +19,32 @@ public class ArrayModifier {
             //command = "decrease"
             if (command.contains("swap")) {
                 //"swap {index1} {index2}" -> "swap 2 3".split(" ") -> ["swap", "2", "3"]
-                int index1 = Integer.parseInt(command.split(" ")[1]); //индексът на първия елемент за размяна
-                int index2 = Integer.parseInt(command.split(" ")[2]); //индексът на втория елемент за размяна
-                //1. взимаме елемента на първия индекс
+                int index1 = Integer.parseInt(command.split(" ")[1]); //the index of the first item to swap
+                int index2 = Integer.parseInt(command.split(" ")[2]); //the index of the second item to swap
+                //1.take the element at the first index
                 int element1 = numbers[index1];
-                //2. взимаме елемента на втория индекс
+                //2.take the element at the second index
                 int element2 = numbers[index2];
-                //3. размяна
+                //3.swap
                 numbers[index1] = element2;
                 numbers[index2] = element1;
             } else if (command.contains("multiply")) {
                 //"multiply {index1} {index2}"
                 int index1 = Integer.parseInt(command.split(" ")[1]); //индексът на първия елемент за умножение
                 int index2 = Integer.parseInt(command.split(" ")[2]); //индексът на втория елемент за умножение
-                //1. взимаме елемента на първия индекс
+                //1.take the element at the first index
                 int element1 = numbers[index1];
-                //2. взимаме елемента на втория индекс
+                //2.take the element at the second index
                 int element2 = numbers[index2];
 
                 int product = element1 * element2;
                 numbers[index1] = product;
             } else if (command.equals("decrease")) {
                 //decrease
-                //обходим всички елементи -> - 1
-                //позиции / индекси -> 0 до последния (дължината на масива - 1)
+                //loop through all the elements -> - 1
+                //positions / indices -> 0 to last (array length - 1)
                 for (int index = 0; index <= numbers.length - 1; index++) {
-                    //текущия елемент -> numbers[index]
+                    //the current element -> numbers[index]
                     //numbers[index]--;
                     //numbers[index] -= 1;
                     numbers[index] = numbers[index] - 1;
@@ -52,15 +53,15 @@ public class ArrayModifier {
             command = scanner.nextLine();
         }
 
-        //отпечатваме: ", "
+        //print: ", "
 
         for (int index = 0; index <= numbers.length - 1 ; index++) {
             int currentNumber = numbers[index];
             if (index != numbers.length - 1) {
-                //не сме на последното число
+                //not on the last number
                 System.out.print(currentNumber + ", ");
             } else { //index == numbers.length - 1
-                //последното число
+                //last number
                 System.out.print(currentNumber);
             }
         }
