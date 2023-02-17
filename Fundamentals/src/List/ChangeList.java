5335bfd3772e839c6d32a0999c31d528ac27850e
Georgi Ivanov <gogata92@pm.me>
2023-02-17 08:38:25 +0100

    Update ChangeList.java

--- Fundamentals/src/List/ChangeList.java
+++ Fundamentals/src/List/ChangeList.java
@@ -8,17 +8,17 @@ import java.util.stream.Collectors;
 public class ChangeList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //1. списък с числа -> входни данни
-        //2. получаваме команди -> "end"
+        //1. list of numbers -> input data
+        //2. until command end -> "end"
         //"Delete {element}" -> "Delete 3"
         //"Insert {element} {position}" -> "Insert 3 1"
 
         List<Integer> numbers = Arrays.stream(scanner.nextLine() //"1 2 3 4 5 5 5 6"
-                        .split(" "))  //["1", "2", "3", "4", "5", "5", "5", "6"] -> масив от текстове
-                .map(Integer::parseInt) //[1, 2, 3, 4, 5, 5, 5, 6] -> масив от цели числа
-                .collect(Collectors.toList()); //{1, 2, 3, 4, 5, 5, 5, 6} -> списък от цели числа
+                        .split(" "))  //["1", "2", "3", "4", "5", "5", "5", "6"] -> an array of texts
+                .map(Integer::parseInt) //[1, 2, 3, 4, 5, 5, 5, 6] -> an array of integers
+                .collect(Collectors.toList()); //{1, 2, 3, 4, 5, 5, 5, 6} -> a list of integers
 
-        String command = scanner.nextLine(); //команди
+        String command = scanner.nextLine(); //commands
         //stop: command == "end"
         //continue: command != "end"
         while (!command.equals("end")) {
